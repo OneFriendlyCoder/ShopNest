75db1ef6e95d6961d5008f81459e043b807a747c
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-24 21:48:55 +0530

    productDetails page 1.0

--- src/app/product/[productId]/ProductDetails.tsx
+++ src/app/product/[productId]/ProductDetails.tsx
@@ -5,8 +5,23 @@ interface ProductDetailsProps{
 }
 
 const ProductDetails: React.FC<ProductDetailsProps> = ({product}) => {
+    const totalRatings = product.reviews.length;
+    const totalRatingSum = totalRatings > 0 ? product.reviews.reduce((acc: number, item: any)=> item.rating + acc, 0): 0;
+    const productRating = totalRatings > 0 ? totalRatingSum / totalRatings : 0;
     return (  
-        <div>Product Details</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+            <div>
+                Images
+            </div>
+            <div className="flex flex-col gap-1 text-dlate-500 text-sm">
+                <h1 className="text-3xl font-medium text-slate-700">{product.name}</h1>
+                <div className="flex items-center gap-2">
+                    {productRating} Rating
+                    <div>{product.reviews.length} Review</div>
+
+                </div>
+            </div>
+        </div>
     );
 }
  
