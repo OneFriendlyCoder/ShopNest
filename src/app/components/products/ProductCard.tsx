a4e6d0d54a5429b39185c338b5ccd2926cf0ac36
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-24 20:33:21 +0530

    product card styling and functionality

--- src/app/components/products/ProductCard.tsx
+++ src/app/components/products/ProductCard.tsx
@@ -1,22 +1,31 @@
 "use client"
 
 import Image from "next/image";
-
+import { TruncateText } from "@/utils/truncatestring";
+import { FormatPrice } from "@/utils/formatPrice";
+import { Rating } from "@material-tailwind/react";
 interface ProductCardProps{
     data: any;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({data}) => {
+const ProductCard: React.FC<ProductCardProps> = ({data}) => {  
+    const totalRatings = data.reviews.length;
+    const totalRatingSum = totalRatings > 0 ? data.reviews.reduce((acc: number, item: any)=> item.rating + acc, 0): 0;
+    const productRating = totalRatings > 0 ? totalRatingSum / totalRatings : 0;
     return (  
         <div className="col-span-1 cursor-pointer border-[1.2px] border-white bg-white rounded-sm p-2 transition hover:scale-105 text-center text-sm">
-            <div className="flex elex-col item-center w-full gap-1">
+            <div className="flex flex-col items-center w-full gap-1">
                 <div className="aspect-square overflow-hidden relative w-full">
                     <Image className="w-full h-full object-contain" alt={data.name} fill src={data.images[0].image}/>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="mt-4">
+                    {TruncateText(data.name)}
+                </div>
+                <div>
+                    {productRating} Ratings
+                </div>
+                <div>{data.reviews.length} Reviews</div>
+                <div className="semi-bold">{FormatPrice(data.price)}</div>
             </div>
         </div>
     );
