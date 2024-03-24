4bb74c4b9806d589a06211af2e36fc0e6c858b6e
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-24 12:54:25 +0530

    basic navbar 2.0 styling

--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -1,12 +1,16 @@
 import Container from "../Container";
 import Link from "next/link";
+import { Merriweather } from "next/font/google";
+
+const merriweather = Merriweather({subsets: ['latin'], weight:['400']})
+
 const Navbar = () => {
     return ( 
         <div className="sticky top-0 w-full bg-gray-700 z-30 shadow-sm">
             <div className="py-4 border-b-[1px]">
                 <Container>
                     <div className="flex items-center justify-between gap-3 md:gap-0">
-                        <Link href="/">ShopNest</Link>
+                        <Link href="/" className={`${merriweather.className} font-bold text-4xl`}>ShopNest</Link>
                         <div className="hidden md:block">Search</div>
                         <div className="flex items-center gap-8 md:gap-12">
                             <div>Cart Count</div>
