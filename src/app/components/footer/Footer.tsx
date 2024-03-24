ecc89017491bff925cacd6a6d4df7f0ba52ee121
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-24 13:41:10 +0530

    footer styling completed

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,8 +1,43 @@
+import { MdFacebook } from "react-icons/md";
+import Container from "../Container";
+import FooterList from "./FooterList";
+import Link from "next/link";
+import { AiFillInstagram, AiFillTwitterCircle, AiFillYoutube } from "react-icons/ai";
+
 const Footer = () => {
     return ( 
-        <>
-            Footer
-        </>
+        <footer className="bg-gray-600 text-white text-sm mt-16">
+            <Container>
+                <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+                    <FooterList>
+                        <h1 className="text-2xl font-bold mb-2">Shop Categories</h1>
+                        <Link href="#">Phones</Link>
+                        <Link href="#">Laptops</Link>
+                        <Link href="#">Desktop</Link>
+                        <Link href="#">Watches</Link>
+                        <Link href="#">TVs</Link>
+                        <Link href="#">Accessories</Link>
+                    </FooterList>
+                    <FooterList>
+                        <h1 className="text-2xl font-bold mb-2">Customer Service</h1>
+                        <Link href="#">Contact Us</Link>
+                        <Link href="#">Shipping Policy</Link>
+                        <Link href="#">Returns & Exchanges</Link>
+                        <Link href="#">FAQs</Link>
+                    </FooterList>
+                    <FooterList>
+                        <h1 className="text-2xl font-bold mb-2">Follow us</h1>
+                        <div className="flex gap-2">
+                            <Link href="#"><MdFacebook size={24}/></Link>
+                            <Link href="#"><AiFillTwitterCircle size={24}/></Link>
+                            <Link href="#"><AiFillInstagram size={24}/></Link>
+                            <Link href="#"><AiFillYoutube size={24}/></Link>
+                        </div>
+                    </FooterList>
+                </div>
+            </Container>
+            
+        </footer>
     );
 }
 
