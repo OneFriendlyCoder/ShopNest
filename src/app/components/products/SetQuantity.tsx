f190d6f3559891387967a82d4355b63d9fc58f5c
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-26 17:30:01 +0530

    Product Details page color component logic + styling

---
+++ src/app/components/products/SetQuantity.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+
+interface SetQtyProps{
+    cartCounter?: boolean,
+    cartProduct: CartProductType,
+    handleQtyIncrease: () => void;
+    handleQtyDecrease: () => void;
+}
+
+const btnstyles = 'border-[1.2px] border-slate-300 px-2 rounded'
+
+const SetQuantity: React.FC<SetQtyProps> = ({
+    cartCounter,
+    cartProduct,
+    handleQtyIncrease,
+    handleQtyDecrease,
+}) => {
+    return (  
+        <div className="flex gap-8 items-center">
+            {cartCounter ? null: <div className="font-semibold">QUANTITY:</div>}
+            <div className="flex gap-4 items-center text-base">
+                <button onClick={handleQtyDecrease} className={btnstyles}>-</button>  {/* handleQtyIncrease is directly used as the event handler and no additional logic or argument is required for the funciton to execute */}
+                <div>{cartProduct.quantity}</div>
+                <button onClick={handleQtyIncrease} className={btnstyles}>+</button>
+            </div>
+        </div>
+    );
+}
+ 
+export default SetQuantity;
--- src/app/product/[productId]/ProductDetails.tsx
+++ src/app/product/[productId]/ProductDetails.tsx
@@ -1,25 +1,87 @@
 "use client"
-
+import Image from "next/image";
+import { useState, useCallback } from "react";
+import SetColor from "@/app/components/products/SetColor";
 interface ProductDetailsProps{
     product: any;
 }
 
+export type CartProductType = {
+    id: string,
+    name: string,
+    description: string,
+    category: string,
+    brand: string,
+    selectedImage: SelectedImgType,
+    quantity: number,
+    price: number,
+}
+
+export type SelectedImgType = {
+    color: string,
+    colorCode: string,
+    image: string
+}
+
+const Horizontal = () => {
+    return (
+        <hr className="w-[30%] my-2"></hr>
+    )
+}
+
 const ProductDetails: React.FC<ProductDetailsProps> = ({product}) => {
+
+    const [cartProduct, setCartProduct] = useState<CartProductType>({
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        category: product.category,
+        brand: product.brand,
+        selectedImage: {...product.images[0]},
+        quantity: 1,
+        price: product.price,
+    })
+
     const totalRatings = product.reviews.length;
     const totalRatingSum = totalRatings > 0 ? product.reviews.reduce((acc: number, item: any)=> item.rating + acc, 0): 0;
     const productRating = totalRatings > 0 ? totalRatingSum / totalRatings : 0;
+
+    const handleColorSelect = useCallback((value: SelectedImgType)=>{
+        setCartProduct((prev) => {
+            return {...prev, selectedImage: value}
+        })
+    }, [cartProduct.selectedImage])
+
+    
     return (  
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
             <div>
-                Images
+                Image
             </div>
             <div className="flex flex-col gap-1 text-dlate-500 text-sm">
                 <h1 className="text-3xl font-medium text-slate-700">{product.name}</h1>
                 <div className="flex items-center gap-2">
                     {productRating} Rating
                     <div>{product.reviews.length} Review</div>
-
                 </div>
+                <Horizontal />
+                <div className="text-justify">{product.description}</div>
+                <Horizontal />
+                <div>
+                    <span className="font-semibold">CATEGORY: </span>{product.category}
+                </div>
+                <div>
+                    <span className="font-semibold">BRAND: </span>{product.brand}
+                </div>
+                <div className={product.inStock ? 'text-teal-400' : 'text-rose-400'}>
+                    {product.inStock ? 'In stock' : 'Out of stock'}
+                </div>
+                <Horizontal />
+                <SetColor cartProduct={cartProduct} images={product.images} handleColorSelect={handleColorSelect}/>
+                <Horizontal />
+                <div>QUANTITY</div>
+                <Horizontal />
+                <div>add to cart</div>
             </div>
         </div>
     );
