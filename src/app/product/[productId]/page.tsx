965f6b4157b45c6b8eb1f8caa09d2cce7cef123f
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-27 12:57:05 +0530

    rating and comment components for the Product Page done

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,7 +1,7 @@
 import Container from "@/app/components/Container";
 import ProductDetails from "./ProductDetails";
 import { product } from "@/utils/product";
-
+import ListRating from "./ListingRating";
 interface IParams{
     productId?: string;                                                 //optional productId
 }
@@ -12,6 +12,10 @@ const Product = ({params}:{params:IParams}) => {                        //from t
         <div className="p-8">
             <Container>
                 <ProductDetails product={product}/>
+                <div className="flex flex-col mt:20 gap-4">
+                    <div>Add Rating</div>
+                    <ListRating product={product}/>
+                </div>
             </Container>
         </div>
     );
