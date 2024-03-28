e67043db4b999c2b926e75f2c5efd336fb58fe6f
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-28 19:57:43 +0530

    Register form setup

---
+++ src/app/register/RegisterForm.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Heading from "../components/Heading";
+const RegisterForm = () => {
+    return (  
+        <>
+            <Heading title="SignIn"/>
+            <hr className="bg-slate-300 w-full h-px" />
+        </>
+    );
+}
+ 
+export default RegisterForm;
