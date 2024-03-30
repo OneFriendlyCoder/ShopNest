d47048fb9c9bd93a54a368bf6a8ba1d9894cfb34
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-30 11:28:56 +0530

    Login and Register Form basic structure

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -8,29 +8,24 @@ import { FieldValues,useForm, SubmitHandler } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
-const RegisterForm = () => {
+
+const LoginForm = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const {register, handleSubmit, formState: {errors}} = useForm<FieldValues>({defaultValues: {name:"", email: "",password:""}});
-    
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true);
-        console.log(data);
-    }
+    const {register, handleSubmit, formState: {errors}} = useForm<FieldValues>({defaultValues: {email:"", password:""}});
 
     return (  
         <>
-            <Heading title="SignUp"/>
-            <Button outline label="Sign up with Google" icon={AiOutlineGoogle} onClick={()=>{}}/>
+            <Heading title="SignIn"/>
+            <Button outline label="Continue with Google" icon={AiOutlineGoogle} onClick={()=>{}}/>
             <hr className="bg-slate-300 w-full h-px" />
-            <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required/>
             <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required/>
             <Input id="password" label="Password" disabled={isLoading} register={register} errors={errors} required type="password"/>
-            <Button label={isLoading ? 'Loading' : 'SignUp'} onClick={handleSubmit(onSubmit)}/>
-            <p className="text-sm">Already have an account?
-                <Link className="underline" href="/login"> Login</Link>
+            <Button label={isLoading ? 'Loading' : 'Login'} onClick={handleSubmit()}/>
+            <p className="text-sm">Don't have an account?
+                <Link className="underline" href="/register"> Sign Up</Link>
             </p>
         </>
     );
 }
  
-export default RegisterForm;
+export default LoginForm;
