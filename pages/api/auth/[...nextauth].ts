8aa64984f96edb5da2af2ad4aa45c4b93d45a2be
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-03-31 13:10:58 +0530

    login logic complete

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,13 @@
 //defining primsa client to be used by next-auth for authentication, this client is needed to be used globally in the app
 
-import NextAuth from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "../../../libs/prismadb"
 import bcrypt from 'bcrypt'
 
-export default NextAuth({
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -41,5 +41,7 @@ export default NextAuth({
       strategy: 'jwt'
     },
     secret: process.env.NEXTAUTH_SECRET
-});
+}
+
+export default NextAuth(authOptions);
 
--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -8,10 +8,32 @@ import { FieldValues,useForm, SubmitHandler } from "react-hook-form";
 import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 
 const LoginForm = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const {register, handleSubmit, formState: {errors}} = useForm<FieldValues>({defaultValues: {email:"", password:""}});
+    const router = useRouter();
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+       setIsLoading(true);
+       signIn('credentials', {
+        ...data,
+        redirect: false
+       }).then((callback)=>{
+        setIsLoading(false);
+        if(callback?.ok){
+            router.push('/cart');
+            router.refresh();
+            toast.success("Logged In");
+        }
+        if(callback?.error){
+            toast.error(callback.error);
+        }
+       }); 
+    }
 
     return (  
         <>
@@ -20,7 +42,7 @@ const LoginForm = () => {
             <hr className="bg-slate-300 w-full h-px" />
             <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required/>
             <Input id="password" label="Password" disabled={isLoading} register={register} errors={errors} required type="password"/>
-            <Button label={isLoading ? 'Loading' : 'Login'} onClick={()=>{}}/>
+            <Button label={isLoading ? 'Loading' : 'Login'} onClick={()=>{onSubmit}}/>
             <p className="text-sm">Don't have an account?
                 <Link className="underline" href="/register"> Sign Up</Link>
             </p>
