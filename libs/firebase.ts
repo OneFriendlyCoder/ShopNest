7b590ac62d5ac28d2773ad72f05e005d52dd1db2
OneFriendlyCoder <abhyanand311099@gmail.com>
2024-04-02 16:09:54 +0530

    firebase setup for data storage and product models in prisma

---
+++ libs/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAn902OnOVHGHa1bCR8iBg9TxZKVaD-PHA",
+  authDomain: "shopnest-111f7.firebaseapp.com",
+  projectId: "shopnest-111f7",
+  storageBucket: "shopnest-111f7.appspot.com",
+  messagingSenderId: "280707562238",
+  appId: "1:280707562238:web:3cf4f45e7c745511f703af"
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+export default firebaseapp;
